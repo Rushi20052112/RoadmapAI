d14c41a3218e959beb1b563c78c404f700c18900
Rushi20052112 <rushikeshrepale746@gmail.com>
2026-05-31 13:28:46 +0530

    oAuth fixed

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -9,7 +9,7 @@ export const initPassport = () => {
             {
                 clientID: process.env.GOOGLE_CLIENT_ID,
                 clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                callbackURL: 'http://roadmapai-ebw2.onrender.com/api/auth/google/callback',
+                callbackURL: 'https://roadmapai-ebw2.onrender.com/api/auth/google/callback',
             },
             async (accessToken, refreshToken, profile, done) => {
                 try {
