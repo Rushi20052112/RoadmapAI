d87140e414112eb4fa9c544f7fa79838325e8863
Rushi20052112 <rushikeshrepale746@gmail.com>
2026-05-31 13:52:09 +0530

    cors issue solved

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const PORT=process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: ["https://roadmap-ai-gamma.vercel.app", "http://localhost:5173"], 
     credentials: true
 }));
 
